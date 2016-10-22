69568b6e5b0fbb51eabf1a77c8a7c9023e89f663
Ben Su <bensu1013@hotmail.com>
2016-10-21 23:28:15 -0400

    assets

--- CreepGame/GameScene.swift
+++ CreepGame/GameScene.swift
@@ -8,13 +8,19 @@
 
 import SpriteKit
 import GameplayKit
+import UIKit
+
 
 class GameScene: SKScene {
     
-
+    var startPanPosition: CGFloat?
     
     override func didMove(to view: SKView) {
+        let panGesture: UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panCamera))
+        panGesture.minimumNumberOfTouches = 1
+        self.view?.addGestureRecognizer(panGesture)
         
+        self.camera = self.childNode(withName: "sceneCamera") as? SKCameraNode
         
     }
     
@@ -22,4 +28,31 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    func panCamera(_ sender: UIPanGestureRecognizer) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startPanPosition = touches.first?.location(in: self.view).x
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let moveAmount = startPanPosition! - (touches.first?.location(in: self.view).x)!
+        self.camera?.position.x += moveAmount
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startPanPosition = nil
+    }
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
